2ced71fc63b95d95085c9d6dd6461f09e3bd5ec9
Rayza Geandra <RayzaGeandra>
2026-01-11 02:23:00 -0300

    Alteração do nome da classe

--- src/ConversorDeMoedas.java
+++ src/ConversorDeMoedas.java
@@ -1,7 +1,7 @@
-public class conversor {
+public class ConversorDeMoedas {
     public static void main(String[] args) {
         System.out.println("Seja bem-vindo(a) ao conversor de moedas!");
         System.out.println("Escolha a opção desejada no menu abaixo: ");
-        
+
     }
 }
